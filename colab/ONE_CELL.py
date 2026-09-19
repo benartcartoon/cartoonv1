9e47a7c2a2c5cfc9ba1fb398cc46491c2961d8c1
benartcartoon <benartaydemir@gmail.com>
2026-09-19 16:23:53 +0300

    Fix WAN2.2 Colab startup and skip flash-attn build

--- colab/ONE_CELL.py
+++ colab/ONE_CELL.py
@@ -1,6 +1,7 @@
 from google.colab import drive
 import os
-if not os.path.ismount("/content/drive"): drive.mount("/content/drive")
+if not os.path.ismount("/content/drive"):
+    drive.mount("/content/drive")
 !rm -rf /content/cartoonv1
-!git clone -q https://github.com/benartcartoon/cartoonv1.git /content/cartoonv1
+!git clone -q --depth 1 https://github.com/benartcartoon/cartoonv1.git /content/cartoonv1
 !bash /content/cartoonv1/colab/setup_wan22.sh
