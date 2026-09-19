829c7b12ba51c75aa672bef3279e7272b488e740
benartcartoon <benartaydemir@gmail.com>
2026-09-19 11:10:43 +0300

    Remove old AnimateDiff setup for Wan2.2 migration

--- colab/ONE_CELL.py
+++
@@ -1,8 +0,0 @@
-# Run this ONE cell in a Colab GPU runtime
-from google.colab import drive
-drive.mount('/content/drive')
-!apt-get -qq update
-!apt-get -qq install -y aria2
-!rm -rf /content/cartoonv1
-!git clone -q https://github.com/benartcartoon/cartoonv1.git /content/cartoonv1
-!bash /content/cartoonv1/colab/setup_animatediff.sh
